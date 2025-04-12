17aa375708767bf5c168bf9e23567669b446642a
Nareshkumar Rao <dev@nrx.sh>
2025-04-13 01:18:42 +0200

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use tokio::net::{TcpListener, TcpStream, UdpSocket};
+use tokio::{
+    net::{TcpListener, TcpStream, UdpSocket},
+    sync::mpsc::{Receiver, Sender},
+};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -14,12 +17,34 @@ async fn main() -> anyhow::Result<()> {
         .expect("port not specified")
         .parse::<u16>()
         .expect("port not a number");
-    //local_listener(local_port).await?;
-    puncher(local_port, &remote_addr, remote_port).await?;
+    let peer_port = args
+        .get(4)
+        .expect("port not specified")
+        .parse::<u16>()
+        .expect("port not a number");
+
+    let (tx_to_peer, mut rx_from_peer) = tokio::sync::mpsc::channel::<Vec<u8>>(32);
+    let (tx_to_local, mut rx_from_local) = tokio::sync::mpsc::channel::<Vec<u8>>(32);
+
+    local_listener(peer_port, tx_to_peer, rx_from_local).await?;
+    puncher(
+        local_port,
+        &remote_addr,
+        remote_port,
+        tx_to_local,
+        rx_from_peer,
+    )
+    .await?;
     Ok(())
 }
 
-async fn puncher(local_port: u16, remote_addr: &str, remote_port: u16) -> anyhow::Result<()> {
+async fn puncher(
+    local_port: u16,
+    remote_addr: &str,
+    remote_port: u16,
+    tx: Sender<Vec<u8>>,
+    mut rx: Receiver<Vec<u8>>,
+) -> anyhow::Result<()> {
     let socket = UdpSocket::bind(("0.0.0.0", local_port)).await?;
     socket.connect((remote_addr, remote_port)).await?;
     let mut received_ack = false;
@@ -47,20 +72,62 @@ async fn puncher(local_port: u16, remote_addr: &str, remote_port: u16) -> anyhow
         };
     }
     println!("Punched through to {}:{}", remote_addr, remote_port);
+    loop {
+        socket.readable().await?;
+        let mut buf = [0; 1024];
+        match socket.try_recv(&mut buf) {
+            Ok(n) => {
+                if n == 0 {
+                    println!("Peer disconnected");
+                    break;
+                }
+                tx.send(buf[..n].to_vec()).await?;
+                let response = rx
+                    .recv()
+                    .await
+                    .ok_or(anyhow::anyhow!("Failed to receive response"))?;
+                loop {
+                    match socket.try_send(&response) {
+                        Ok(_) => break,
+                        Err(e) => {
+                            if e.kind() == std::io::ErrorKind::WouldBlock {
+                                continue;
+                            } else {
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            Err(e) => {
+                if e.kind() == std::io::ErrorKind::WouldBlock {
+                    continue;
+                } else {
+                    break;
+                }
+            }
+        }
+    }
 
     Ok(())
 }
 
-async fn local_listener(port: u16) -> anyhow::Result<()> {
+async fn local_listener(
+    port: u16,
+    tx: Sender<Vec<u8>>,
+    rx: Receiver<Vec<u8>>,
+) -> anyhow::Result<()> {
     println!("Listening on port {}", port);
     let listener = TcpListener::bind(("0.0.0.0", port)).await?;
-    loop {
-        let (socket, _) = listener.accept().await?;
-        tokio::spawn(handle_client(socket));
-    }
+    let (socket, _) = listener.accept().await?;
+    handle_client(socket, tx, rx).await
 }
 
-async fn handle_client(socket: TcpStream) -> anyhow::Result<()> {
+async fn handle_client(
+    socket: TcpStream,
+    tx: Sender<Vec<u8>>,
+    mut rx: Receiver<Vec<u8>>,
+) -> anyhow::Result<()> {
     println!("Accepted connection from {}", socket.peer_addr().unwrap());
     loop {
         socket.readable().await?;
@@ -71,7 +138,11 @@ async fn handle_client(socket: TcpStream) -> anyhow::Result<()> {
                     println!("Client disconnected");
                     break;
                 }
-                let response = forward_to_peer(&buf[..n]).await?;
+                tx.send(buf[..n].to_vec()).await?;
+                let response = rx
+                    .recv()
+                    .await
+                    .ok_or(anyhow::anyhow!("Failed to receive response"))?;
                 loop {
                     match socket.try_write(&response) {
                         Ok(_) => break,
@@ -95,11 +166,4 @@ async fn handle_client(socket: TcpStream) -> anyhow::Result<()> {
         }
     }
     Ok(())
-}
-
-async fn forward_to_peer(data: &[u8]) -> anyhow::Result<Vec<u8>> {
-    println!("Forwarding data: {:?}", data);
-    let response = format!("Received: {:?}\n", data);
-    let response = response.as_bytes().to_vec();
-    Ok(response)
 }
